fc3b874227aec0c657a4e1a3c2dc60ae012db04a
AntonovAtanas <atanas.antonov1@gmail.com>
2022-10-13 17:12:09 +0300

    Create 02 - add-and-subtract.js

---
+++ 04 - Exercise - Functions/02 - add-and-subtract.js
@@ -0,0 +1,21 @@
+function addAndSubtract(firstNum, secondNum, thirdNum) {
+
+    function sum(firstNum, secondNum) {
+        let result = firstNum + secondNum;
+        return result;
+    }
+    let add = sum(firstNum, secondNum)
+
+    function subtract(thirdNum) {
+        return thirdNum
+    }
+    let sub = subtract(thirdNum);
+
+    console.log(add - sub)
+}
+
+addAndSubtract(1,
+
+    17,
+    
+    30)
