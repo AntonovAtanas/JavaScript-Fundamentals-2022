0555b86f7ecf9610852b5981aa5c64bfe53264b7
AntonovAtanas <atanas.antonov1@gmail.com>
2022-10-19 16:37:27 +0300

    Create 01 - sum-first-last.js

---
+++ 05 - Lab - Arrays Advanced/01 - sum-first-last.js
@@ -0,0 +1,10 @@
+function sumFirstLast (input){
+
+    let firstNum = Number(input.shift());
+    let lastNum = Number(input.pop());
+    let sum = firstNum + lastNum;
+
+    console.log(sum)
+}
+
+sumFirstLast (['20', '30', '40'])
