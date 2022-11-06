4b69d627e15971d4cd3ba1ef9f7713a6d27160c0
AntonovAtanas <atanas.antonov1@gmail.com>
2022-11-06 17:47:18 +0200

    Create 02 - city.js

---
+++ 06 - Lab - Object and Classes/02 - city.js
@@ -0,0 +1,21 @@
+function cityObj (city){
+
+    for (let key of Object.keys(city)){
+        console.log(`${key} -> ${city[key]}`)
+    }
+
+}
+
+cityObj ({
+
+    name: "Plovdiv",
+    
+    area: 389,
+    
+    population: 1162358,
+    
+    country: "Bulgaria",
+    
+    postCode: "4000"
+    
+    })
