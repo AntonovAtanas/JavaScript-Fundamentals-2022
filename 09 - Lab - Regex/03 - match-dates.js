5cf544f33330584242edaf01046ae56fc84ff4f4
AntonovAtanas <atanas.antonov1@gmail.com>
2022-11-30 17:03:18 +0200

    Create 03 - match-dates.js

---
+++ 09 - Lab - Regex/03 - match-dates.js
@@ -0,0 +1,13 @@
+function phoneNumber(input) {
+
+    let regex = /\b(?<day>\d{2})([-.-\/])(?<month>[A-Z][a-z]{2})\2(?<year>\d{4})\b/g
+    let result = input[0].matchAll(regex);
+
+    for (const match of result) {
+        console.log(`Day: ${match.groups.day}, Month: ${match.groups.month}, Year: ${match.groups.year}`)
+    }
+}
+
+
+phoneNumber(
+    ['13/Jul/1928, 10-Nov-1934, , 01/Jan-1951,f 25.Dec.1937 23/09/1973, 1/Feb/2016'])
