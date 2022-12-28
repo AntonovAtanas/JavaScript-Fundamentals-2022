0b145eff0b8a6f1237f17084991d22eb62d0fde7
AntonovAtanas <atanas.antonov1@gmail.com>
2022-12-29 00:10:08 +0200

    Create 02 - last-k-sequence.js

---
+++ JS Advanced - 2023/02 - Lab - Arrays & Nested Arrays/02 - last-k-sequence.js
@@ -0,0 +1,17 @@
+function lastSequence (n, k) {
+
+    let array = [1];
+
+   for (let i = 1; i < n; i++){
+        let lastK = array.slice(-k);
+        let sum = 0;
+
+        for (const num of lastK) {
+            sum += Number(num)
+        }
+        array.push(sum);
+   }
+    return array;
+}
+
+console.log(lastSequence (6, 3))
