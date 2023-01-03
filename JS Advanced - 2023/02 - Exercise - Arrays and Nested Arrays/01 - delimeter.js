cece661e2510f655a03882101fb0c548d3e63e72
AntonovAtanas <atanas.antonov1@gmail.com>
2023-01-03 21:51:27 +0200

    Create 01 - delimeter.js

---
+++ JS Advanced - 2023/02 - Exercise - Arrays and Nested Arrays/01 - delimeter.js
@@ -0,0 +1,19 @@
+function printByDelimeter (input, delimeter){
+
+    console.log(input.join(delimeter))
+
+}
+
+printByDelimeter (['One',
+
+'Two',
+
+'Three',
+
+'Four',
+
+'Five'],
+
+'-');
+
+printByDelimeter (['How about no?', 'I', 'will', 'not', 'do', 'it!'], '_')
