b6d932d44f5679753e12633b3be91dd2d24eed57
AntonovAtanas <atanas.antonov1@gmail.com>
2023-01-13 23:46:33 +0200

    deleted wrong problem

--- JS Advanced - 2023/04 - Lab - DOM/EditElement.js
+++
@@ -1,9 +0,0 @@
-function editElement(ref, oldText, updatedText) {
-    let text = ref.textContent;
-
-    while(text.includes(oldText)){
-        text = text.replace(oldText, updatedText)
-    }
-
-     ref.textContent = text;
-}
