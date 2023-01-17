ab7177d00eb8f7bd2330690ecaec0e31e352672d
AntonovAtanas <atanas.antonov1@gmail.com>
2023-01-17 21:57:05 +0200

    Create showLess.js

---
+++ JS Advanced - 2023/04 - Lab - DOM/04.Show More/showLess.js
@@ -0,0 +1,5 @@
+function showLess (){
+    document.getElementById('more').style.display = 'inline';
+    document.getElementById('less').style.display = 'none';
+    document.getElementById('text').style.display = 'none';
+}
