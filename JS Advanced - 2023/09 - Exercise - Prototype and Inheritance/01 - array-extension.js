f8461a7f0894692f4b2457189187f50d52a23d34
AntonovAtanas <atanas.antonov1@gmail.com>
2023-03-15 22:08:51 +0200

    Create 01 - array-extension.js

---
+++ JS Advanced - 2023/09 - Exercise - Prototype and Inheritance/01 - array-extension.js
@@ -0,0 +1,22 @@
+(function arrayExtender() {
+    Array.prototype.last = function () {
+        return this[this.length - 1];  
+    };
+
+    Array.prototype.skip = function (n) {
+        return this.slice(n)
+    };
+
+    Array.prototype.take = function (n) {
+        return this.slice(0, n)
+    };
+
+    Array.prototype.sum = function () {
+        return this.reduce((accumulator, currentValue) => accumulator + currentValue, 0)
+    };
+
+    Array.prototype.average = function () {
+        return this.sum() / this.length
+    };
+
+})()
