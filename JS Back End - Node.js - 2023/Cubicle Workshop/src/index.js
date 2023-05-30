dd1b024dc6222e7eabee951c236a2319ae740ee1
AntonovAtanas <atanas.antonov1@gmail.com>
2023-05-30 22:13:12 +0300

    installed mongoose

--- JS Back End - Node.js - 2023/Cubicle Workshop/src/index.js
+++ JS Back End - Node.js - 2023/Cubicle Workshop/src/index.js
@@ -1,22 +1,18 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const expressConfig = require('./config/expressConfig');
 const handlebarsConfig = require('./config/handlebarsConfig');
-
-const homeController = require('./controllers/homeController');
-const cubeController = require('./controllers/cubeController');
-const error404 = require('./controllers/404')
 
 const PORT = 5000;
 
 const app = express();
 
+const routes = require('./routes')
+
 expressConfig(app);
 handlebarsConfig(app);
 
-app.use(homeController);
-app.use('/cube', cubeController);
-app.use('*', error404);
-
+app.use(routes);
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}...`));
---
+++ JS Back End - Node.js - 2023/Cubicle Workshop/src/routes.js
@@ -0,0 +1,11 @@
+const router = require('express').Router();
+
+const homeController = require('./controllers/homeController');
+const cubeController = require('./controllers/cubeController');
+const error404 = require('./controllers/404');
+
+router.use(homeController);
+router.use('/cube', cubeController);
+router.use('*', error404);
+
+module.exports = router;
