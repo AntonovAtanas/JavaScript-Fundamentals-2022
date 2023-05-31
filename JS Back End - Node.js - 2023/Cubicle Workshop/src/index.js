b8f69717a30e780b42c5ea8531c6337628e1fd33
AntonovAtanas <atanas.antonov1@gmail.com>
2023-05-31 10:48:58 +0300

    Load database and error handling on app init

--- JS Back End - Node.js - 2023/Cubicle Workshop/src/index.js
+++ JS Back End - Node.js - 2023/Cubicle Workshop/src/index.js
@@ -1,14 +1,18 @@
 const express = require('express');
-const mongoose = require('mongoose');
 
 const expressConfig = require('./config/expressConfig');
 const handlebarsConfig = require('./config/handlebarsConfig');
+const dbConnect = require('./config/dbConfig');
 
 const PORT = 5000;
 
 const app = express();
 
 const routes = require('./routes')
+
+dbConnect()
+    .then(() => console.log('Db has connected successfully.'))
+    .catch((err) => console.log(`Failed to connect to db - ${err}`))
 
 expressConfig(app);
 handlebarsConfig(app);
