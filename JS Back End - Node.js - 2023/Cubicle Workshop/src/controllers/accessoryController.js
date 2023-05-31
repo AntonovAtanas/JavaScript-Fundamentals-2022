6043f07798a4085ca02a2a581a5552cfeb0b90e2
AntonovAtanas <atanas.antonov1@gmail.com>
2023-05-31 21:40:36 +0300

    Add attach accessory page with data

--- JS Back End - Node.js - 2023/Cubicle Workshop/src/controllers/accessoryController.js
+++ JS Back End - Node.js - 2023/Cubicle Workshop/src/controllers/accessoryController.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 
+const cubeManager = require('../managers/cubeManager');
 const accessoryManager = require('../managers/accessoryManager');
 
 router.get('/create', (req, res) => {
@@ -8,11 +9,23 @@ router.get('/create', (req, res) => {
 });
 
 router.post('/create', (req, res) => {
-    const newAccessory = {...req.body};
+    const newAccessory = { ...req.body };
 
     accessoryManager.addAccessory(newAccessory)
 
     res.redirect('/');
 });
+
+router.get('/attach/:id', async (req, res) => {
+    const cubeId = req.params.id;
+
+    const cube = await cubeManager.getCube(cubeId).lean();
+    const accessories = await accessoryManager.getAll().lean();
+
+    console.log(cube);
+    console.log(accessories);
+
+    res.render('./accessory/attachAccessory', { cube, accessories });
+})
 
 module.exports = router;
