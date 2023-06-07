a6cdeb21a4316024df2ea3d3358fd7036181f2e6
AntonovAtanas <atanas.antonov1@gmail.com>
2023-06-07 20:56:38 +0300

    Add register page

--- JS Back End - Node.js - 2023/Cubicle Workshop/src/routes.js
+++ JS Back End - Node.js - 2023/Cubicle Workshop/src/routes.js
@@ -3,11 +3,13 @@ const router = require('express').Router();
 const homeController = require('./controllers/homeController');
 const cubeController = require('./controllers/cubeController');
 const accessoryController = require('./controllers/accessoryController');
+const userController = require('./controllers/userController');
 const error404 = require('./controllers/404');
 
 router.use(homeController);
 router.use('/cube', cubeController);
 router.use('/accessory', accessoryController);
+router.use('/user', userController);
 router.use('*', error404);
 
 module.exports = router;
