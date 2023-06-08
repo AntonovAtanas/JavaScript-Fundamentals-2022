d2b18587b6cdcd029752efe138317f116e0267b6
AntonovAtanas <atanas.antonov1@gmail.com>
2023-06-08 10:56:51 +0300

    Added login functinality with attaching auth token

--- JS Back End - Node.js - 2023/Cubicle Workshop/src/controllers/userController.js
+++ JS Back End - Node.js - 2023/Cubicle Workshop/src/controllers/userController.js
@@ -1,12 +1,12 @@
 const router = require('express').Router();
 const userManager = require('../managers/userManager');
-
+const cookieParser = require('cookie-parser');
 
 router.get('/register', (req, res) => {
     res.render('./user/registerPage');
 });
 
-router.post('/register', (req, res) => {
+router.post('/register', async (req, res) => {
 
     const { username, password, repeatPassword } = req.body;
 
@@ -15,12 +15,23 @@ router.post('/register', (req, res) => {
         throw new Error('Passwords do not match');
     }
 
-    userManager.register({ username, password });
+    await userManager.register({ username, password });
     res.redirect('/');
-})
+});
 
 router.get('/login', (req, res) => {
     res.render('./user/loginPage');
+});
+
+router.post('/login', async (req, res) => {
+
+    const {username, password} = req.body;
+
+    const token = await userManager.login(username, password);
+
+    res.cookie('auth', token, {httpOnly: true});
+
+    res.redirect('/');
 })
 
 module.exports = router;
--- JS Back End - Node.js - 2023/Cubicle Workshop/src/managers/userManager.js
+++ JS Back End - Node.js - 2023/Cubicle Workshop/src/managers/userManager.js
@@ -1,4 +1,32 @@
 const User = require('../models/User');
+const jwtoken = require('../lib/jwtoken');
+const bcrypt = require('bcrypt');
+const { MongooseError } = require('mongoose');
+
+const SECRET = 'b42f3f6f796fa3cfcb6b1b8d49c7bcccc3a7164c';
 
 exports.register = (userData) => User.create(userData);
+
+exports.login = async (username, password) => {
+    const user = await User.findOne({username}).lean();
+    
+    if (!user) {
+        throw new MongooseError('Username or password do not match');
+    };
+
+    const isValid = await bcrypt.compare(password, user.password);
+    
+    if (!isValid) {
+        throw new MongooseError('Username or password do not match');
+    };
+
+    const payload = {
+        _id: user._id,
+        username: user.username
+    };
+
+    const token = await jwtoken.sign(payload, SECRET);
+
+    return token;
+}
 
