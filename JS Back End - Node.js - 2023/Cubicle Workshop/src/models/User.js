c1f3e0fdb6e1c3835877d5eed661d904e5a9a50a
AntonovAtanas <atanas.antonov1@gmail.com>
2023-06-08 09:44:43 +0300

    Add hash password functionality

--- JS Back End - Node.js - 2023/Cubicle Workshop/src/models/User.js
+++ JS Back End - Node.js - 2023/Cubicle Workshop/src/models/User.js
@@ -1,9 +1,15 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
     username: String,
     password: String
 });
+
+userSchema.pre('save', async function() {
+    const hashedPassword = await bcrypt.hash(this.password, 10);
+    this.password = hashedPassword;
+})
 
 const User = mongoose.model('User', userSchema);
 
