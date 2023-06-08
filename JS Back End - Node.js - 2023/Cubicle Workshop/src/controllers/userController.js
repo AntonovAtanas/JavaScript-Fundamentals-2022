607b93dc6d86cf1e7c2263fb30df60503f797aaa
AntonovAtanas <atanas.antonov1@gmail.com>
2023-06-08 09:33:27 +0300

    Add register feature

--- JS Back End - Node.js - 2023/Cubicle Workshop/src/controllers/userController.js
+++ JS Back End - Node.js - 2023/Cubicle Workshop/src/controllers/userController.js
@@ -1,8 +1,22 @@
 const router = require('express').Router();
+const userManager = require('../managers/userManager');
 
 
 router.get('/register', (req, res) => {
     res.render('./user/registerPage');
+});
+
+router.post('/register', (req, res) => {
+
+    const { username, password, repeatPassword } = req.body;
+
+    if (password !== repeatPassword) {
+        res.redirect('/404')
+        throw new Error('Passwords do not match');
+    }
+
+    userManager.register({ username, password });
+    res.redirect('/');
 })
 
 router.get('/login', (req, res) => {
