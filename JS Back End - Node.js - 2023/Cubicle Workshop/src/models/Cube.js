94e49b4b79575c399a959feda58f935a54afd8ac
AntonovAtanas <atanas.antonov1@gmail.com>
2023-06-11 17:43:38 +0300

    Added mongoose validation for Cube and Accessory

--- JS Back End - Node.js - 2023/Cubicle Workshop/src/models/Cube.js
+++ JS Back End - Node.js - 2023/Cubicle Workshop/src/models/Cube.js
@@ -1,9 +1,20 @@
 const mongoose = require('mongoose');
 
 const cubeSchema = new mongoose.Schema({
-    name: String,
-    description: String,
-    imageUrl: String,
+    name: {
+        type: String,
+        match: [/^[A-Za-z0-9 ]+$/, 'Invalid name characters used.'],
+        minLength: [5, 'Name must be minimum 5 characters.']
+    },
+    description: {
+        type: String,
+        match: [/^[A-Za-z0-9 ]+$/, 'Invalid description characters used'],
+        minLength: [20, 'Description must be minimum 20 characters']
+    },
+    imageUrl: {
+        type: String,
+        match: [/^https:\/\/|http:\/\//, 'Invalid image URL']
+    },
     difficultyLevel: Number,
     accessories: [{
         type: mongoose.Types.ObjectId,
