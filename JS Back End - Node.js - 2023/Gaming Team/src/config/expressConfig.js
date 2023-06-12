7b6b78d2f96c8313c6bcf8d79a8ea345ea535486
AntonovAtanas <atanas.antonov1@gmail.com>
2023-06-12 14:35:08 +0300

    Add body parser middleware

--- JS Back End - Node.js - 2023/Gaming Team/src/config/expressConfig.js
+++ JS Back End - Node.js - 2023/Gaming Team/src/config/expressConfig.js
@@ -2,7 +2,8 @@ const express = require('express');
 
 
 function expressConfig(app){
-    app.use(express.static('src/public'))
+    app.use(express.static('src/public'));
+    app.use(express.urlencoded({extended: false}));
 }
 
 module.exports = expressConfig
