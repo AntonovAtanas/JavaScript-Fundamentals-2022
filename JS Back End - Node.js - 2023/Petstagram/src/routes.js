368ef14453c58b8090b195237cb355953a20173c
AntonovAtanas <atanas.antonov1@gmail.com>
2023-06-14 15:46:41 +0300

    Render catalog page

--- JS Back End - Node.js - 2023/Petstagram/src/routes.js
+++ JS Back End - Node.js - 2023/Petstagram/src/routes.js
@@ -2,10 +2,12 @@ const router = require('express').Router();
 
 const homeController = require('./controllers/homeController');
 const userController = require('./controllers/userController');
+const petsController = require('./controllers/petsController');
 const error404Controller = require('./controllers/error404Controller');
 
 router.use(homeController);
 router.use('/user', userController);
+router.use(petsController);
 router.use('*', error404Controller);
 
 module.exports = router;
