13143de98f768ab158f11dc900348f46ee191f1c
AntonovAtanas <atanas.antonov1@gmail.com>
2023-06-15 15:07:33 +0300

    Create Game model

---
+++ JS Back End - Node.js - 2023/Gaming Team/src/models/Game.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+
+const gameSchema = new mongoose.Schema({
+    platform: {
+        type: String,
+        // VALIDATION??
+        required: [true, 'Platform is required']
+    },
+    name: {
+        type: String,
+        minLength: [4, 'Name must be minimum 4 characters'],
+        required: [true, 'Name is required']
+    },
+    image: {
+        type: String,
+        match: [/^https?:\/\//, 'Image must start with http or https'],
+        required: [true, 'Image is required']
+    },
+    price: {
+        type: Number,
+        required: [true, 'Price is required'],
+        min: 1
+    },
+    genre: {
+        minLength: [2, 'Genre must be minimum 2 characters long'],
+        required: [true, 'Genre is required']
+    },
+    description: {
+        minLength: [10, 'Description must be minimum 10 characters long'],
+        required: [true, 'Description is required']
+    },
+    owner: {
+        type: mongoose.Types.ObjectId,
+        ref: 'User'
+    },
+    boughtBy: [{
+        type: mongoose.Types.ObjectId,
+        ref: 'User'
+    }]
+});
+
+const Game = mongoose.model('Game', gameSchema);
+
+model.exports = Game;
