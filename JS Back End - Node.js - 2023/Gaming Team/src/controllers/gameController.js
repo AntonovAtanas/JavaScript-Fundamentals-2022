a16d8274bfa5c12edf7491740bc578d2408d1d39
AntonovAtanas <atanas.antonov1@gmail.com>
2023-06-15 15:31:07 +0300

    Render all games

--- JS Back End - Node.js - 2023/Gaming Team/src/controllers/gameController.js
+++ JS Back End - Node.js - 2023/Gaming Team/src/controllers/gameController.js
@@ -1,22 +1,33 @@
 const router = require('express').Router();
 
-const gameManager = require('../managers/gameManager')
+const gameManager = require('../managers/gameManager');
+const { errorMessageHandler } = require('../utils/errorMessageHandler')
+
+router.get('/catalog', async (req, res) => {
+
+    try {
+        const allGames = await gameManager.allGames().lean();
+        res.render('./games/catalog', { allGames });
+    } catch (error) {
+        return res.render('./games/catalog', { errorMessage: errorMessageHandler(error) })
+    }
+})
 
 router.get('/create', (req, res) => {
     res.render('./games/create');
 })
 
-router.post('/create', async(req, res) => {
+router.post('/create', async (req, res) => {
     const gameDetails = req.body;
     const userId = req.user?._id
 
     try {
-        await gameManager.addGame({...gameDetails, owner: userId})
+        await gameManager.addGame({ ...gameDetails, owner: userId })
     } catch (error) {
-        console.log(error)
+        return res.render('./games/create', { errorMessage: errorMessageHandler(error) })
     }
 
-    res.redirect('/')
+    res.redirect('/games/catalog')
 })
 
 module.exports = router;
--- JS Back End - Node.js - 2023/Gaming Team/src/managers/gameManager.js
+++ JS Back End - Node.js - 2023/Gaming Team/src/managers/gameManager.js
@@ -1,3 +1,5 @@
 const Game = require('../models/Game')
 
+exports.allGames = () => Game.find();
+
 exports.addGame = (gameData) => Game.create(gameData)
