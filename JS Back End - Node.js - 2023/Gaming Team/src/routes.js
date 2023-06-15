8b4df261074630c0aa9b5dd1a9874c5ca412b7e5
AntonovAtanas <atanas.antonov1@gmail.com>
2023-06-15 14:54:19 +0300

    Add static create game page

--- JS Back End - Node.js - 2023/Gaming Team/src/routes.js
+++ JS Back End - Node.js - 2023/Gaming Team/src/routes.js
@@ -2,10 +2,12 @@ const router = require('express').Router();
 
 const homeController = require('./controllers/homeController');
 const userController = require('./controllers/userController');
+const gameController = require('./controllers/gameController');
 const error404Controller = require('./controllers/error404Controller');
 
 router.use(homeController);
 router.use('/user', userController);
+router.use('/games', gameController);
 router.use('*', error404Controller);
 
 module.exports = router;
