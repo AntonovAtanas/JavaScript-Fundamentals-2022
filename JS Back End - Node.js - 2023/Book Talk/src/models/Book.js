ca827c144fead4adc3dd05d41b5e8c3882ed3cb1
AntonovAtanas <atanas.antonov1@gmail.com>
2023-06-16 11:46:24 +0300

    Add Book model with validation

---
+++ JS Back End - Node.js - 2023/Book Talk/src/models/Book.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+
+const bookSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: [true, 'Title is required'],
+        minLength: [2, 'Title should be minimum 2 characters']
+    },
+    author: {
+        type: String,
+        required: [true, 'Author is required'],
+        minLength: [5, 'Author should be minimum 5 characters']
+    },
+    image: {
+        type: String,
+        match: [/^https?:\/\//, 'Image must start with http or https'],
+        required: [true, 'Image is required']
+    },
+    review: {
+        type: String,
+        required: [true, 'Review is required'],
+        minLength: [10, 'Review should be minimum 10 characters']
+    },
+    genre: {
+        type: String,
+        required: [true, 'Genre is required'],
+        minLength: [3, 'Genre should be minimum 3 characters']
+    },
+    stars: {
+        type: Number,
+        required: [true, 'Stars is required'],
+        min: [1, 'Rating must be between 1 and 5'],
+        max: [5, 'Rating must be between 1 and 5']
+    },
+    wishlist: [{
+        type: mongoose.Types.ObjectId,
+        ref: 'User'
+    }],
+    owner: {
+        type: mongoose.Types.ObjectId,
+        ref: 'User'
+    }
+})
