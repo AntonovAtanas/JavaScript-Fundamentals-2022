587bd7ace7b7bf206e94dfaf1e7c5d052738b8f0
AntonovAtanas <atanas.antonov1@gmail.com>
2023-06-17 11:14:59 +0300

    Add static home page

---
+++ JS Back End - Node.js - 2023/Crypto Trade/src/config/config.js
@@ -0,0 +1,2 @@
+exports.SECRET = '38a23134-ecff-4607-b5b7-872a05e8b8ad';
+
---
+++ JS Back End - Node.js - 2023/Crypto Trade/src/middlewares/authMiddleware.js
@@ -0,0 +1,31 @@
+const jwtoken = require('../lib/jwt');
+const { SECRET } = require('../config/config');
+
+exports.auth = async (req, res, next) => {
+    const token = req.cookies['auth'];
+
+    if (token) {
+        try {
+            const decodedToken = await jwtoken.verify(token, SECRET);
+
+            req.user = decodedToken;
+
+            res.locals.isAuthenticated = true;
+
+            next();
+        } catch (error) {
+            res.clearCookie('auth');
+            res.redirect('/user/login');
+        }
+    } else {
+        next();
+    }
+};
+
+exports.isAuth = (req, res, next) => {
+    if (!req.user) {
+        return res.redirect('/users/login');
+    }
+
+    next();
+};
