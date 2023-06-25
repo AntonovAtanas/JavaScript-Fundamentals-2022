39fd7d8d5afd4c2ee2f9f6fbb183eea9e212ca43
AntonovAtanas <atanas.antonov1@gmail.com>
2023-06-25 12:17:24 +0300

    Solved Components - lab

---
+++ Angular/first-angular/src/app/data/seed.ts
@@ -0,0 +1,43 @@
+const data = [
+  {
+    title: 'Article 1',
+    description:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the indust when an unknown printer took a galley of type and scrambled it to make a type specimen book.It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.It was popularised in the 1960s with the release o dummy text of the printing and typesetting industry. Lorem Ipsum has been the indust whedummy text of the printing and typesetting industry. Lorem Ipsum has been the indust whedummy text of the printing and typesetting industry. Lorem Ipsum has been the indust whedummy text of the printing and typesetting industry. Lorem Ipsum has been the indust whe',
+    author: 'Author 1',
+    imageUrl:
+      'http://ichef.bbci.co.uk/news/976/cpsprodpb/10434/production/_90121666_agreementcartoon.jpg',
+  },
+  {
+    title: 'Article 2',
+    description:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the indust when an unknown printer took a galley of type and scrambled it to make a type specimen book.It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.It was popularised in the 1960s with the release o dummy text of the printing and typesetting industry. Lorem Ipsum has been the indust whedummy text of the printing and typesetting industry. Lorem Ipsum has been the indust whedummy text of the printing and typesetting industry. Lorem Ipsum has been the indust whedummy text of the printing and typesetting industry. Lorem Ipsum has been the indust whe',
+    author: 'Author 2',
+    imageUrl:
+      'http://www.digitalmeetsculture.net/wp-content/uploads/2015/04/article.jpg',
+  },
+  {
+    title: 'Article 3',
+    description:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the indust when an unknown printer took a galley of type and scrambled it to make a type specimen book.It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.It was popularised in the 1960s with the release o dummy text of the printing and typesetting industry. Lorem Ipsum has been the indust whedummy text of the printing and typesetting industry. Lorem Ipsum has been the indust whedummy text of the printing and typesetting industry. Lorem Ipsum has been the indust whedummy text of the printing and typesetting industry. Lorem Ipsum has been the indust whe',
+
+    author: 'Author 3',
+    imageUrl:
+      'https://media.istockphoto.com/id/1322277517/photo/wild-grass-in-the-mountains-at-sunset.jpg?s=612x612&w=0&k=20&c=6mItwwFFGqKNKEAzv0mv6TaxhLN3zSE43bWmFN--J5w=',
+  },
+  {
+    title: 'Article 4',
+    description:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the indust when an unknown printer took a galley of type and scrambled it to make a type specimen book.It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.It was popularised in the 1960s with the release o dummy text of the printing and typesetting industry. Lorem Ipsum has been the indust whedummy text of the printing and typesetting industry. Lorem Ipsum has been the indust whedummy text of the printing and typesetting industry. Lorem Ipsum has been the indust whedummy text of the printing and typesetting industry. Lorem Ipsum has been the indust whe',
+    author: 'Author 4',
+    imageUrl: 'https://www.seoclerk.com/pics/518476-1iDFBR1489198900.png',
+  },
+  {
+    title: 'Article 5',
+    description:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the indust when an unknown printer took a galley of type and scrambled it to make a type specimen book.It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.It was popularised in the 1960s with the release o dummy text of the printing and typesetting industry. Lorem Ipsum has been the indust whedummy text of the printing and typesetting industry. Lorem Ipsum has been the indust whedummy text of the printing and typesetting industry. Lorem Ipsum has been the indust whedummy text of the printing and typesetting industry. Lorem Ipsum has been the indust whe',
+    author: 'Author 5',
+    imageUrl:
+      'http://www.stat.columbia.edu/~gelman/teaching/literacy/images/IV.gif',
+  },
+];
+export { data };
