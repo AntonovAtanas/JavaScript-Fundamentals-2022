6340ba5c3dc2219b13744901c68380271b96a689
AntonovAtanas <atanas.antonov1@gmail.com>
2023-07-03 14:52:31 +0300

    Add dynamic rendering of all themes

--- Angular/SoftUni-Forum/src/app/core/main-container/themes/themes.component.ts
+++ Angular/SoftUni-Forum/src/app/core/main-container/themes/themes.component.ts
@@ -1,19 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from 'src/app/services/api.service';
 
+import {Theme} from '../../../interfaces/Theme'
+
 @Component({
   selector: 'app-themes',
   templateUrl: './themes.component.html',
   styleUrls: ['./themes.component.css'],
 })
 export class ThemesComponent implements OnInit {
-  allThemes = [];
+  allThemes: Theme[] = [];
 
   constructor(private api: ApiService) {}
 
   ngOnInit(): void {
     this.api.getThemes().subscribe(res => {
-      console.log(res)
+      this.allThemes = [...res]
     })
   }
 }
--- Angular/SoftUni-Forum/src/app/services/api.service.ts
+++ Angular/SoftUni-Forum/src/app/services/api.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { api } from '../environmentals/server-url';
 
 import { HttpClient } from '@angular/common/http';
+import { Theme } from '../interfaces/Theme';
 
 @Injectable({
   providedIn: 'root',
@@ -10,6 +11,6 @@ export class ApiService {
   constructor(private http: HttpClient) {}
 
   getThemes() {
-    return this.http.get(`${api.serverUrl}/themes`)
+    return this.http.get<Theme[]>(`${api.serverUrl}/themes`)
   };
 }
