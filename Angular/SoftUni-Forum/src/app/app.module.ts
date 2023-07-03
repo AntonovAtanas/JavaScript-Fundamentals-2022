d9ca6d229ca276396f38f0054f6aa0e1c0796563
AntonovAtanas <atanas.antonov1@gmail.com>
2023-07-03 11:57:56 +0300

    Refactored components to have single responsibility

--- Angular/SoftUni-Forum/src/app/app.module.ts
+++ Angular/SoftUni-Forum/src/app/app.module.ts
@@ -2,16 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { NavigationComponent } from './navigation/navigation.component';
-import { MainContainerComponent } from './main-container/main-container.component';
-import { FooterComponent } from './footer/footer.component';
+import { NavigationComponent } from './core/navigation/navigation.component';
+import { MainContainerComponent } from './core/main-container/main-container.component';
+import { FooterComponent } from './core/footer/footer.component';
+import { ThemesComponent } from './core/main-container/themes/themes.component';
+import { AsideComponent } from './core/main-container/aside/aside.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavigationComponent,
     MainContainerComponent,
-    FooterComponent
+    FooterComponent,
+    ThemesComponent,
+    AsideComponent
   ],
   imports: [
     BrowserModule
