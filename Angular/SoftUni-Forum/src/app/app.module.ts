2bf8f5ea14d7a8b39be6cd6727727f30275e4b02
AntonovAtanas <atanas.antonov1@gmail.com>
2023-07-03 11:22:47 +0300

    Added navigation and styles

--- Angular/SoftUni-Forum/src/app/app.module.ts
+++ Angular/SoftUni-Forum/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { MainContainerComponent } from './main-container/main-container.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    MainContainerComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
