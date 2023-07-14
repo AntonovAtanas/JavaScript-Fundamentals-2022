42309d6b5bf3512db9a8360c49a7d15e717a8af0
AntonovAtanas <atanas.antonov1@gmail.com>
2023-07-14 16:13:41 +0300

    Add subscribe button if user logged

--- Angular/SoftUni-Forum/src/app/themes/all-themes/themes.component.ts
+++ Angular/SoftUni-Forum/src/app/themes/all-themes/themes.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ApiService } from 'src/app/services/api.service';
 
 import { Theme } from '../../interfaces/Theme';
+import { UserService } from 'src/app/user/user.service';
 
 @Component({
   selector: 'app-themes',
@@ -11,12 +12,16 @@ import { Theme } from '../../interfaces/Theme';
 export class ThemesComponent implements OnInit {
   allThemes: Theme[] = [];
 
-  constructor(private api: ApiService) {}
+  constructor(private api: ApiService, private userService: UserService) {}
 
   ngOnInit(): void {
     this.api.getThemes().subscribe((res) => {
       res.sort((a, b) => b.subscribers.length - a.subscribers.length);
       this.allThemes = [...res];
     });
+  }
+
+  get isLogged(): boolean {
+    return this.userService.isLogged;
   }
 }
