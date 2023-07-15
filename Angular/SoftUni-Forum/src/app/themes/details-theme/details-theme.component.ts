0ae3c22f1b226fd3b1ce1c3f718141cbd1366919
AntonovAtanas <atanas.antonov1@gmail.com>
2023-07-15 17:11:59 +0300

    Add comment option for logged in users

--- Angular/SoftUni-Forum/src/app/themes/details-theme/details-theme.component.ts
+++ Angular/SoftUni-Forum/src/app/themes/details-theme/details-theme.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Theme } from 'src/app/interfaces/Theme';
 import { ApiService } from 'src/app/services/api.service';
+import { UserService } from 'src/app/user/user.service';
 
 @Component({
   selector: 'app-details-theme',
@@ -12,10 +13,18 @@ export class DetailsThemeComponent implements OnInit {
 
   currentTheme: Theme | undefined;
 
-  constructor(private api: ApiService, private activatedRoute: ActivatedRoute) {}
+  constructor(private api: ApiService, private activatedRoute: ActivatedRoute, private userService: UserService) {}
 
   ngOnInit(): void {
     const id = this.activatedRoute.snapshot.params['themeId'];
     this.api.getTheme(id).subscribe(res => this.currentTheme = res);
+  }
+
+  get isLogged(): boolean{
+    return this.userService.isLogged;
+  }
+
+  get username(): string {
+    return this.userService.user?.username;
   }
 }
