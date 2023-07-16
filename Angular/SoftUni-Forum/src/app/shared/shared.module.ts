af01eee519f105aeac92bc2da07eb7225dc9a869
AntonovAtanas <atanas.antonov1@gmail.com>
2023-07-16 14:57:16 +0300

    Fixed home component routing error

--- Angular/SoftUni-Forum/src/app/shared/shared.module.ts
+++ Angular/SoftUni-Forum/src/app/shared/shared.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
+import { UserRoutingModule } from '../user/user-routing.module';
 
 
 
 @NgModule({
   declarations: [HomeComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    UserRoutingModule
   ],
   exports: [HomeComponent]
 })
