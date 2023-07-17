f3e2c3c91c193083af2e8e9bc0821d83dbb7f597
AntonovAtanas <atanas.antonov1@gmail.com>
2023-07-17 17:52:45 +0300

    Add register form with default values

--- Angular/Forms-Playground/forms/src/app/register-form/register-form.component.ts
+++ Angular/Forms-Playground/forms/src/app/register-form/register-form.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-register-form',
   templateUrl: './register-form.component.html',
   styleUrls: ['./register-form.component.css']
 })
-export class RegisterFormComponent {
+export class RegisterFormComponent implements AfterViewInit {
 
+  @ViewChild ('f') registerForm!: NgForm
+
+  jobs: string[] = ['Designer', 'Programmer', 'Artist'];
+  defaultJob: string = 'Programmer';
+
+  phoneCodes: string[] = ['+359', '+07', '+02'];
+  defaultCode: string = '+359';
+
+
+
+  ngAfterViewInit(): void {
+    // console.log(`After View Init`, this.registerForm)
+  }
+
+  onSubmit(): void{
+    console.log(`onSubmit`, this.registerForm.form.value);
+
+    this.registerForm.reset()
+  }
 }
