03ca873f45a4a0f9a8d9b210f895bb359b5ef9be
AntonovAtanas <atanas.antonov1@gmail.com>
2023-07-18 10:57:54 +0300

    Add password grouping validation

--- Angular/Forms-Playground/forms/src/app/register-form/register-form.component.ts
+++ Angular/Forms-Playground/forms/src/app/register-form/register-form.component.ts
@@ -1,14 +1,13 @@
-import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-register-form',
   templateUrl: './register-form.component.html',
-  styleUrls: ['./register-form.component.css']
+  styleUrls: ['./register-form.component.css'],
 })
 export class RegisterFormComponent {
-
-  @ViewChild ('f') registerForm!: NgForm
+  @ViewChild('f') registerForm!: NgForm;
 
   jobs: string[] = ['Designer', 'Programmer', 'Artist'];
   defaultJob: string = 'Programmer';
@@ -16,7 +15,7 @@ export class RegisterFormComponent {
   phoneCodes: string[] = ['+359', '+07', '+02'];
   defaultCode: string = '+359';
 
-  onSubmit(): void{
-    this.registerForm.reset()
+  onSubmit(): void {
+    this.registerForm.reset();
   }
 }
