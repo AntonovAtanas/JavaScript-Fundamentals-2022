63ad96899c4ce8881382c97d624c742475de62ec
AntonovAtanas <atanas.antonov1@gmail.com>
2023-07-19 12:07:48 +0300

    Add reactive form submittion

--- Angular/Forms-Playground/forms/src/app/register-form-reactive/register-form-reactive.component.ts
+++ Angular/Forms-Playground/forms/src/app/register-form-reactive/register-form-reactive.component.ts
@@ -9,15 +9,23 @@ import { FormControl, FormGroup } from '@angular/forms';
 export class RegisterFormReactiveComponent implements OnInit {
   registerForm!: FormGroup;
 
+  jobs: string[] = ['Designer', 'Programmer', 'Artist'];
+
+  phoneCodes: string[] = ['+359', '+07', '+02', '+034'];
+
   ngOnInit(): void {
     this.registerForm = new FormGroup({
       'name': new FormControl(null),
       'email': new FormControl(null),
-      'code': new FormControl(null),
-      'tel': new FormControl(null),
-      'job': new FormControl(null),
+      'code': new FormControl('+02'),
+      'tel': new FormControl(),
+      'job': new FormControl('Programmer'),
       'password': new FormControl(null),
       'rePassword': new FormControl(null),
     })
+  };
+
+  onSubmit(){
+    console.log(this.registerForm)
   }
 }
