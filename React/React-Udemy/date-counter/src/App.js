de834c1e458c20f5bab877f80a85adb4cbf2fb54
AntonovAtanas <atanas.antonov1@gmail.com>
2023-10-22 23:14:54 +0300

    solved date counter task

--- React/React-Udemy/date-counter/src/App.js
+++ React/React-Udemy/date-counter/src/App.js
@@ -1,26 +1,33 @@
-import { useState} from 'react'
+import { useState } from "react";
 
-import './App.css';
-
+import "./App.css";
 
 function App() {
+  const [step, setStep] = useState(1);
+  const [count, setCount] = useState(0);
+
+  let date = new Date('2023-10-22');
+  date.setDate(date.getDate() + count)
+
   return (
     <div className="App">
-      <Step />
+      <div className="step">
+        <button onClick={() => (step > 0 ? setStep(step - 1) : setStep(step))}>
+          -
+        </button>
+        <p>Step: {step}</p>
+        <button onClick={() => setStep(step + 1)}>+</button>
+      </div>
+      <div className="count">
+        <button onClick={() => setCount(count - step)}>-</button>
+        <p>Count: {count}</p>
+        <button onClick={() => setCount(count + step)}>+</button>
+      </div>
+      <div className="result">
+        <p>{count} days from today is {date.toLocaleDateString()}</p>
+      </div>
     </div>
   );
-}
-
-function Step () {
-  const [step, setStep] = useState(1)
-
-  return (
-    <div className="step">
-      <button>-</button>
-      <p>Step: {step}</p>
-      <button>+</button>
-    </div>
-  )
 }
 
 export default App;
