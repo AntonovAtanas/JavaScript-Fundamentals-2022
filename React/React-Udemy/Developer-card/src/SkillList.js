08040950d0c62bd26e3c3fd4ece74d64d5cd21cb
AntonovAtanas <atanas.antonov1@gmail.com>
2023-10-22 13:58:03 +0300

    Update SkillList.js

--- React/React-Udemy/Developer-card/src/SkillList.js
+++ React/React-Udemy/Developer-card/src/SkillList.js
@@ -1,54 +1,62 @@
 const skills = [
-    {
-      skill: "HTML+CSS",
-      level: "advanced",
-      color: "#2662EA"
-    },
-    {
-      skill: "JavaScript",
-      level: "advanced",
-      color: "#EFD81D"
-    },
-    {
-      skill: "Web Design",
-      level: "advanced",
-      color: "#C3DCAF"
-    },
-    {
-      skill: "Git and GitHub",
-      level: "intermediate",
-      color: "#E84F33"
-    },
-    {
-      skill: "React",
-      level: "advanced",
-      color: "#60DAFB"
-    },
-    {
-      skill: "Svelte",
-      level: "beginner",
-      color: "#FF3B00"
-    }
-  ];
+  {
+    skill: "HTML+CSS",
+    level: "advanced",
+    color: "#2662EA",
+  },
+  {
+    skill: "JavaScript",
+    level: "advanced",
+    color: "#EFD81D",
+  },
+  {
+    skill: "Web Design",
+    level: "advanced",
+    color: "#C3DCAF",
+  },
+  {
+    skill: "Git and GitHub",
+    level: "intermediate",
+    color: "#E84F33",
+  },
+  {
+    skill: "React",
+    level: "advanced",
+    color: "#60DAFB",
+  },
+  {
+    skill: "Svelte",
+    level: "beginner",
+    color: "#FF3B00",
+  },
+];
 
 function SkillList() {
   return (
     <div className="skill-list">
-      <Skill skills={skills}/>
+      <Skill skills={skills} />
     </div>
   );
 }
 
-function Skill({skills}) {
-  return (
-    skills.map(skill => {
-        return (
-            <div className="skill" style={{ backgroundColor: skill.color }} key={skill.skill}>
-                {`${skill.skill} ${skill.level === 'beginner' ? '👦' : skill.level === 'intermediate' ? '👍' : '💪'}`}
-            </div>
-        )
-       }) 
-  );
+function Skill({ skills }) {
+  return skills.map((skill) => {
+    return (
+      <div
+        className="skill"
+        style={{ backgroundColor: skill.color }}
+        key={skill.skill}
+      >
+        {`${skill.skill} ${
+          skill.level === "beginner"
+            ? "👦"
+            : skill.level === "intermediate"
+            ? "👍"
+            : "💪"
+        }`}
+      </div>
+    );
+  });
 }
 
 export default SkillList;
